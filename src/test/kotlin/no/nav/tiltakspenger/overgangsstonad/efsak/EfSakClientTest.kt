a5d3f55eb19c85d57e96c0b277defa8cc7d6988d
Erlend Vige <erlend.edvardsen.vige@nav.no>
2023-01-24 15:54:25 +0100

    Lagt til flere tester i EfSakClientTest

--- src/test/kotlin/no/nav/tiltakspenger/overgangsstonad/efsak/EfSakClientTest.kt
+++ src/test/kotlin/no/nav/tiltakspenger/overgangsstonad/efsak/EfSakClientTest.kt
@@ -2,12 +2,14 @@ package no.nav.tiltakspenger.overgangsstonad.efsak
 
 import io.ktor.client.engine.mock.MockEngine
 import io.ktor.client.engine.mock.respond
+import io.ktor.client.plugins.ClientRequestException
 import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpStatusCode
 import io.ktor.http.headersOf
 import kotlinx.coroutines.test.runTest
 import no.nav.tiltakspenger.overgangsstonad.httpClientGeneric
+import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Test
 
 internal class EfSakClientTest {
@@ -34,13 +36,32 @@ internal class EfSakClientTest {
     fun `EF Sak svarer 200 og personen har ikke overgangsstønad`() {
         val mockEngine = MockEngine {
             respond(
-                content = ""
+                content = """{"data":{"perioder":[],"status":"test","melding":"test","frontendFeilmelding":"test","stacktrace":"test"}}""".trimMargin(),
+                status = HttpStatusCode.OK,
+                headers = headersOf(HttpHeaders.ContentType, ContentType.Application.Json.toString())
             )
+        }
+        val client = httpClientGeneric(mockEngine)
+        val efSakClient = EfSakClient(client) { "token for testing" }
+        runTest {
+            val response = efSakClient.hentOvergangsstønad("ident", "fom", "tom", "behovId")
+            assert(response.data.perioder.size == 0)
         }
     }
 
     @Test
     fun `EF Sak svarer 400 Bad Request og det kastes exception`() {
-
+        val mockEngine = MockEngine {
+            respond(
+                content = """400 Bad Request""".trimMargin(),
+                status = HttpStatusCode.BadRequest,
+                headers = headersOf(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+            )
+        }
+        val client = httpClientGeneric(mockEngine)
+        val efSakClient = EfSakClient(client) { "token for testing" }
+        assertThrows(ClientRequestException::class.java) {
+            runTest { efSakClient.hentOvergangsstønad("ident", "fom", "tom", "behovId") }
+        }
     }
 }
